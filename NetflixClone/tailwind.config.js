7b0f543b1b11d9956cbf37ae1ac2874c0c5c4dcb
Felipe C <83608036+dFLPp@users.noreply.github.com>
2022-03-04 20:15:48 -0300

    adding #1

---
+++ NetflixClone/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'homePage': "url('https://pipocasclub.com.br/wp-content/uploads/2019/10/netflix-3.jpg')",
+      }
+    },
+  },
+  plugins: [],
+}
